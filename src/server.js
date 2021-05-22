3e292a6ddbc90d3ebe6d63f1c4bb0aa21d7549d9
iam-dante <koreanhoseon@gmail.com>
2021-05-22 15:48:54 +0900

    Update server

--- src/server.js
+++ src/server.js
@@ -5,13 +5,10 @@ const PORT = 4000;
 const app = express();
 
 const handleHome = (req, res) => {
-  return res.send("I still love you.");
+  return res.send("Hello!");
 };
-const handleLogin = (req, res) => {
-  return res.send("Login here.");
-};
-app.get("/", handleHome);
-app.get("/login", handleLogin);
+
+app.get('/', handleHome);
 
 const handleListening = () =>
   console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
