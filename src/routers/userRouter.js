a1169ad747aa5f2e83de6f8e0fb4be1a2022927f
iam-dante <koreanhoseon@gmail.com>
2021-05-23 20:47:58 +0900

    Divide router and controller

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -2,9 +2,9 @@ import express from "express";
 
 
 const userRouter = express.Router();
-const handleEditUser = (req, res) => res.send("Edit User");
 
 userRouter.get("/edit", handleEditUser);
+userRouter.get("/delete", handleDelete);
 
 
 export default userRouter;
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,9 +1,8 @@
 import express from "express";
 
-
 const videoRouter = express.Router();
-const handleWatchVideo = (req, res) => res.send("Watch Video");
 
 videoRouter.get("/watch", handleWatchVideo);
+videoRouter.get("/edit", handleEdit);
 
 export default videoRouter;
