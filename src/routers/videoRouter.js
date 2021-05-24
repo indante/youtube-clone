b5e7224b705995d9ef02f37230245c65a9536cf5
iam-dante <koreanhoseon@gmail.com>
2021-05-24 17:14:01 +0900

    Add regular expression

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,8 +1,10 @@
 import express from "express";
-
+import { see, edit, upload, deleteVideo } from "../controllers/videoController";
 const videoRouter = express.Router();
 
-videoRouter.get("/watch", handleWatchVideo);
-videoRouter.get("/edit", handleEdit);
+videoRouter.get("/upload", upload);
+videoRouter.get("/:id(\\d+)", see);
+videoRouter.get("/:id(\\d+)/edit", edit);
+videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 
 export default videoRouter;
