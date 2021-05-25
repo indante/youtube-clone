5153790450a54e621814c220acec2b66774f40a7
iam-dante <koreanhoseon@gmail.com>
2021-05-25 23:26:54 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,8 +4,6 @@ import globalRouter from "./routers/globalRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
 
-const PORT = 4000;
-
 const app = express();
 const logger = morgan("dev");
 
@@ -17,8 +15,4 @@ app.use("/", globalRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
 
-
-const handleListening = () =>
-  console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
-
-app.listen(PORT, handleListening);
+export default app;
