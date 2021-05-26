6277c72b29c4b3fe56b4039c36061cc74b15f396
iam-dante <koreanhoseon@gmail.com>
2021-05-26 19:05:28 +0900

    Update userController

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const postJoin = async (req, res) => {
@@ -19,18 +20,48 @@ export const postJoin = async (req, res) => {
       errorMessage: "This username/email is already taken."
     });
   };
+  try {
+    await User.create({
+      name,
+      username,
+      email,
+      password,
+      location,
+    });
+    return res.redirect("/login");
+  } catch (error) {
+    return res.status(400).render("join", {
+      pageTitle: "Join", 
+      errorMessage: error._message,
+    });
+  }
+};
 
-  await User.create({
-    name,
-    username,
-    email,
-    password,
-    location,
-  });
-  return res.redirect("/login");
+export const getLogin = (req, res) => res.render("Login", { pageTitle: "Login" });
+
+export const postLogin = async (req, res) => {
+  const { username, password } = req.body;
+  const pageTitle = "Login";
+  const user = await User.findOne({ username });
+
+  if (!user) {
+    return res.status(400).render("login", {
+      pageTitle,
+      errorMessage: "An account with this username does not exists.",
+    });
+  };
+
+  const comparePassword = await bcrypt.compare(password, user.password); // compare to post password and user.password
+  if (!comparePassword) {
+    return res.status(400).render("login", {
+      pageTitle,
+      errorMessage: "Wrong password",
+    });
+  };
+  console.log("LOG USER IN! COMING SOON!");
+  return res.redirect("/");
 };
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
-export const login = (req, res) => res.send("Log In");
 export const logout = (req, res) => res.send("Log Out");
 export const see = (req, res) => res.send("See User");
