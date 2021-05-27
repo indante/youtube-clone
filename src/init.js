2c9422b3f118b294d8316704db4a9445e182530c
iam-dante <koreanhoseon@gmail.com>
2021-05-27 18:13:52 +0900

    Update init.js
    import dotenv

--- src/init.js
+++ src/init.js
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import "./db";
 import "./models/Video";
 import "./models/User";
@@ -9,4 +10,3 @@ const handleListening = () =>
   console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
 
 app.listen(PORT, handleListening);
-
