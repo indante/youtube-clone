6e10430d69cd9240507ee2e13b4424a1e6059586
iam-dante <koreanhoseon@gmail.com>
2021-05-27 18:14:33 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import morgan from "morgan";
 import session from "express-session";
+import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
+import { localsMiddleware } from "./middlewares";
 
 const app = express();
 const logger = morgan("dev");
@@ -14,12 +16,20 @@ app.use(logger);
 app.use(express.urlencoded({ extended: true }));
 
 app.use(session({
-    secret: "Hello!",
-    resave: true,
-    saveUninitialized: true,
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
   })
 );
 
+app.use((req, res, next) => {
+  req.sessionStore.all((error, sessions) => {
+    next();
+  });
+});
+
+app.use(localsMiddleware);
 app.use("/", rootRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
