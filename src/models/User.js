837fdbc20de88468515867896a6b6602a309c78f
iam-dante <koreanhoseon@gmail.com>
2021-05-27 20:16:28 +0900

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -1,20 +1,19 @@
-import bcrypt from "bcrypt"; // hashing password
+import bcrypt from "bcrypt";
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
+  avatarUrl: String,
+  socialOnly: { type: Boolean, default: false },
   username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  password: { type: String },
   name: { type: String, required: true },
   location: String,
 });
 
-
-userSchema.pre('save', async function() { // Mongoose Middleware
-  // console.log("Users password:", this.password); // before
+userSchema.pre("save", async function () {
   this.password = await bcrypt.hash(this.password, 5);
-  // console.log("Hashed password", this.password); // after
 });
 
-const User = mongoose.model('User', userSchema)
+const User = mongoose.model("User", userSchema);
 export default User;
