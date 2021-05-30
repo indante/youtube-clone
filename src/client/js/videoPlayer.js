139c0ed1a00b98e716da233133094baa4f0ddf1d
iam-dante <koreanhoseon@gmail.com>
2021-05-30 21:55:23 +0900

    Update videoPlayer

---
+++ src/client/js/videoPlayer.js
@@ -0,0 +1,26 @@
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const time = document.getElementById("time");
+const volume = document.getElementById("volume");
+
+const handlePlayClick = (e) => {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+  playBtn.innerText = video.paused ? "Play" : "Pause";
+};
+
+const handleMute = (e) => {
+  if (video.muted) {
+    video.muted = false;
+  } else {
+    video.muted = true;
+  }
+  muteBtn.innerText = video.muted ? "Unnute" : "Mute";
+};
+
+play.addEventListener("click", handlePlayClick);
+mute.addEventListener("click", handleMute);
