54dd16cdf29b47ffd9b23a5537857270826d68a6
iam-dante <koreanhoseon@gmail.com>
2021-06-03 17:39:00 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,37 +1,37 @@
 import express from "express";
 import morgan from "morgan";
 import session from "express-session";
+import flash from "express-flash";
 import MongoStore from "connect-mongo";
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
+import apiRouter from "./routers/apiRouter";
 import { localsMiddleware } from "./middlewares";
 
 const app = express();
 const logger = morgan("dev");
 
-app.set("view engine", "pug");
-app.set("views", process.cwd() + "/src/views");
-app.use(logger);
-app.use(express.urlencoded({ extended: true }));
-
-app.use(session({
+app.set("view engine", "pug"); // 템플릿 엔진을 pug로 사용한다고 선언
+app.set("views", process.cwd() + "/src/views"); // express path 바꾸기
+app.use(logger); // req, res를 보기 좋게 formatting 해주는 모듈
+app.use(express.urlencoded({ extended: true })); // req.body 데이터를 받아오기 위함
+app.use(express.json());
+app.use(
+  session({
     secret: process.env.COOKIE_SECRET,
     resave: false,
     saveUninitialized: false,
     store: MongoStore.create({ mongoUrl: process.env.DB_URL }),
   })
 );
-
-app.use((req, res, next) => {
-  req.sessionStore.all((error, sessions) => {
-    next();
-  });
-});
-
+app.use(flash());
 app.use(localsMiddleware);
+app.use("/uploads", express.static("uploads"));
+app.use("/static", express.static("assets"));
 app.use("/", rootRouter);
-app.use("/users", userRouter);
 app.use("/videos", videoRouter);
+app.use("/users", userRouter);
+app.use("/api", apiRouter);
 
 export default app;
